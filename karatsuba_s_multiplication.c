b92cd325795509b3d7279f964545de5b59dcb069
S0R3d <tommasoguidi55@gmail.com>
2022-02-14 11:53:41 +0100

    Fix bug of carry-over

--- karatsuba_s_multiplication.c
+++ karatsuba_s_multiplication.c
@@ -27,39 +27,33 @@ void number_to_digits(int n, int *s, int dim) {
 // Function that prints digits in a split
 void print_digits(int *s, int dim) {
    for (size_t i = 0; i < dim; i++)
-   {
       printf("pos %zu: %d\n", i, *(s+i));
-   }
    printf("\n");
 }
 
 // Function that reduces number in a split (2+ digits) in digits on the same split
 // - It takes into account the carry-overs given by the sum
-void carry_over_digits(int *s, int dim) {
+void carry_over_to_digits(int *s, int dim) {
    for (size_t i = 0; i < dim; i++) {
       int dimN = floor(log10(abs(s[i])))+1;
       if (dimN != 1) {
          int r = *(s+i)%10;
          int d = *(s+i)/10;
-         *(s+i) = r;
-         *(s+(i+1)) += d;
+
+         if (i != dim-1) {
+            *(s+i) = r;
+            *(s+(i+1)) += d;
+         }
       }
    }
 }
 
 // Function that sums digits value from 2 splits and store in the first one
 void sum_digits(int *s1, int *s2, int dim) {
-   // FIXME: +=: a livello di bit all'interno di un 'int' per *s non viene sovrascritto il valore
-   //             ma shiftato a destra per ogni somma.
-   // *s risulta 2,8,16,10,1,4,8,5
    for (size_t i = 0; i < dim; i++)
       *(s1+i) += *(s2+i);
-   
-   // FIXME: errore here add 1 in pos 4
-   printf("s1 in: \n");
-   print_digits(s1, dim*2);
 
-   carry_over_digits(s1, dim);
+   carry_over_to_digits(s1, dim);
 }
 
 // K-Multiplication of 2/1 digits in splits
@@ -70,12 +64,12 @@ void K_multip_2dig(int *sum, int *s1, int dim1, int *s2, int dim2)
    *(sum+2) = *(s1+ dim1 -1) * *(s2+ dim2 -1);
    *(sum+3) = 0;
 
-   carry_over_digits(sum, dim1+dim2);
+   carry_over_to_digits(sum, dim1+dim2);
 }
 
 // K-Multiplication of 4/3, 2/1 digits in splits
-// TODO: incorret value of second more sign. digit in 'sum', for 4 digits sum;
-void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2) {
+void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2) 
+{
    if (dim1 != 2 && dim2 != 2) {
       K_multip(sum, s1, dim1/2, s2, dim2/2);
 
@@ -86,26 +80,12 @@ void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2) {
 
       K_multip(sumapp, s1, dim1/2, &s2[dim2/2], dim2-dim2/2);
 
-      printf("sumapp\n");
-      print_digits(sumapp, dim1+1);
-
       K_multip(sumapp1, &s1[dim1/2], dim1-dim1/2, s2, dim2/2);
 
-      // TODO: Provare a sommare 0 prima di sommare in sumapp
-      // TODO: Provare con array normali, ma non si puo modificare la lunghezza a runtime
-      printf("sumapp1\n");
-      print_digits(sumapp1, dim1);
+      sum_digits(sumapp, sumapp1, dim1);
 
-      // FIXME: 5th digit wrong, in sum_digits
-      sum_digits(sumapp, sumapp1, dim1+1);
-
-      printf("sumapp sum \n");
-      print_digits(sumapp, dim1+1);
-
-      printf("sum \n");
-      print_digits(sum, dim1+dim2);
-
-      sum_digits(&sum[(dim1+dim2)/4], sumapp, dim1+1);
+      sum_digits(&sum[(dim1+dim2)/4], sumapp, dim1);
+      // carry_over_to_digits(sum, dim1+dim2);
 
       free(sumapp);
       free(sumapp1);
@@ -114,9 +94,10 @@ void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2) {
 
 // Function that receves input numbers from user
 void user_input(int* n1, int* n2) {
-   printf("Insert first number (range ~1000 - 100 ; 99 - 10 ; 9 - 1): ");
+   printf("Numbers Range: 0, 1 - 9, 10 - 99, 100 - 999, 1000 - 9999 \n");
+   printf("Insert first number: ");
    scanf("%d", n1);
-   printf("Insert second number (range ~1000 - 100 ; 99 - 10 ; 9 - 1): ");
+   printf("Insert second number: ");
    scanf("%d", n2);
 }
 
@@ -126,20 +107,16 @@ void user_input(int* n1, int* n2) {
 int main() {
    int n1,n2;
    
-   // user_input(&n1, &n2);
-   n1 = 5999, n2 = 5999;
+   user_input(&n1, &n2);
+   // n1 = 8888, n2 = 8888;
 
    int p = 0;
 
    int dim1 = floor(log10(abs(n1)))+1;
    int dim2 = floor(log10(abs(n2)))+1;
 
-   if (dim1%2 != 0) {
-      dim1 += 1;
-   }
-   if (dim2%2 != 0) {
-      dim2 += 1;
-   }
+   dim1 += dim1%2 != 0 ? 1 : 0;
+   dim2 += dim2%2 != 0 ? 1 : 0;
 
    int* sum = (int*)malloc(sizeof(int) * (dim1+dim2));
 
@@ -155,16 +132,13 @@ int main() {
    
    printf("--------------------\n");
    for (int i = 0; i < (dim1+dim2); i++)
-   {
       printf("sum: %d\n", sum[i]);
-   }
    printf("--------------------\n");
 
    for (int i = 0; i < (dim1+dim2); i++)
-   {
       p += (sum[i] * (int)pow(10,(double)i));
-   }
    
+
    printf("Result:      %d\n", p);
    printf("Correct:     %d\n", n1*n2);
    printf("Difference:  %d\n", (n1*n2) - p);
