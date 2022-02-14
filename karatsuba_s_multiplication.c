bf14f8c34052841b781cff28137d1e9a1f2ab464
S0R3d <tommasoguidi55@gmail.com>
2022-02-14 18:10:30 +0100

    Update K_Multip: Number type int->int64_t, Dimension type int->size_t, add new func check_sizes() and add if-statem. to prevent function loops

--- karatsuba_s_multiplication.c
+++ karatsuba_s_multiplication.c
@@ -4,10 +4,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 
 // Function that converts a number in digits and store in split (less sign. digit -> less sign. posizion)
-void R_number_to_digits(int n, int *s, int dim) {
+void R_number_to_digits(int n, int *s, size_t dim) {
    for (size_t i = 0; i < dim; i++)
    {
       *(s+i) = n%10;
@@ -16,7 +17,7 @@ void R_number_to_digits(int n, int *s, int dim) {
 }
 
 // Function that converts a number in digits and store in a split (more sign. digit -> less sign. posizion)
-void number_to_digits(int n, int *s, int dim) {
+void number_to_digits(int n, int *s, size_t dim) {
    for (size_t i = dim; i-- > 0; )
    {
       *(s+i) = n%10;
@@ -25,17 +26,29 @@ void number_to_digits(int n, int *s, int dim) {
 }
 
 // Function that prints digits in a split
-void print_digits(int *s, int dim) {
+void print_digits(int *s, size_t dim) {
    for (size_t i = 0; i < dim; i++)
       printf("pos %zu: %d\n", i, *(s+i));
    printf("\n");
 }
 
+// Function that controll if 'dimension' is even and if isn't add 1
+void check_sizes(size_t* ptr1, size_t* ptr2) {
+   if (*ptr1 != *ptr2 && *ptr1 > *ptr2)
+      *ptr2 += (*ptr1) - (*ptr2);
+   if (*ptr1 != *ptr2 && *ptr2 > *ptr1)
+      *ptr1 += (*ptr2) - (*ptr1);
+   if ((*ptr1)%2 != 0)
+      *ptr1 += 1;
+   if ((*ptr2)%2 != 0)
+      *ptr2 += 1;
+}
+
 // Function that reduces number in a split (2+ digits) in digits on the same split
 // - It takes into account the carry-overs given by the sum
-void carry_over_to_digits(int *s, int dim) {
+void carry_over_to_digits(int *s, size_t dim) {
    for (size_t i = 0; i < dim; i++) {
-      int dimN = floor(log10(abs(s[i])))+1;
+      size_t dimN = floor(log10(abs(s[i])))+1;
       if (dimN != 1) {
          int r = *(s+i)%10;
          int d = *(s+i)/10;
@@ -49,7 +62,7 @@ void carry_over_to_digits(int *s, int dim) {
 }
 
 // Function that sums digits value from 2 splits and store in the first one
-void sum_digits(int *s1, int *s2, int dim) {
+void sum_digits(int *s1, int *s2, size_t dim) {
    for (size_t i = 0; i < dim; i++)
       *(s1+i) += *(s2+i);
 
@@ -57,7 +70,7 @@ void sum_digits(int *s1, int *s2, int dim) {
 }
 
 // K-Multiplication of 2/1 digits in splits
-void K_multip_2dig(int *sum, int *s1, int dim1, int *s2, int dim2)
+void K_multip_2dig(int *sum, int *s1, size_t dim1, int *s2, size_t dim2)
 {
    *(sum) = *s1 * *s2;
    *(sum+1) = ( *(s1) * *(s2+ dim2 -1) ) + ( *(s1+ dim1 -1) * *(s2) );
@@ -68,8 +81,17 @@ void K_multip_2dig(int *sum, int *s1, int dim1, int *s2, int dim2)
 }
 
 // K-Multiplication of 4/3, 2/1 digits in splits
-void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2) 
+void K_multip(int *sum, int *s1, size_t dim1, int *s2, size_t dim2) 
 {
+   if (dim1 == 0 && dim2 == 0)
+   {
+      fprintf(stderr, "ERROR: possible loop detected!\nDimension value: %zu, %zu\n", dim1,dim2);
+      exit(1);
+   }
+
+   check_sizes(&dim1,&dim2);
+   printf("dim1:%zu dim2:%zu\n", dim1, dim2);
+   
    if (dim1 != 2 && dim2 != 2) {
       K_multip(sum, s1, dim1/2, s2, dim2/2);
 
@@ -85,7 +107,6 @@ void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2)
       sum_digits(sumapp, sumapp1, dim1);
 
       sum_digits(&sum[(dim1+dim2)/4], sumapp, dim1);
-      // carry_over_to_digits(sum, dim1+dim2);
 
       free(sumapp);
       free(sumapp1);
@@ -93,30 +114,30 @@ void K_multip(int *sum, int *s1, int dim1, int *s2, int dim2)
 }
 
 // Function that receves input numbers from user
-void user_input(int* n1, int* n2) {
+void user_input(int64_t* n1, int64_t* n2) {
    printf("Numbers Range: 0, 1 - 9, 10 - 99, 100 - 999, 1000 - 9999 \n");
    printf("Insert first number: ");
-   scanf("%d", n1);
+   scanf("%lld", n1);
    printf("Insert second number: ");
-   scanf("%d", n2);
+   scanf("%lld", n2);
 }
 
-// TODO: change type of ALL number and digits in uint32_t?(per aumentare il numero massimo possibile)
-// size_t, long long int, int32_t, uint32_t, int64_t, uint64_t, intmax_t
+// TODO: funzione da chiamare dall'esterno che fa funzionare K_Multip senza il main
+// Trasforma i numeri in array di digits e regola le dimensioni, alla fine raggruppa i digits nel risultato
+void between_main_multip() {}
 
 int main() {
-   int n1,n2;
-   
-   user_input(&n1, &n2);
-   // n1 = 8888, n2 = 8888;
+   int64_t n1,n2;
 
-   int p = 0;
+   // user_input(&n1, &n2);
+   n1 = 99999, n2 = 99999;
 
-   int dim1 = floor(log10(abs(n1)))+1;
-   int dim2 = floor(log10(abs(n2)))+1;
+   int64_t p = 0;
 
-   dim1 += dim1%2 != 0 ? 1 : 0;
-   dim2 += dim2%2 != 0 ? 1 : 0;
+   size_t dim1 = floor(log10(llabs(n1)))+1;
+   size_t dim2 = floor(log10(llabs(n2)))+1;
+
+   check_sizes(&dim1, &dim2);
 
    int* sum = (int*)malloc(sizeof(int) * (dim1+dim2));
 
@@ -126,22 +147,27 @@ int main() {
    int* s2 = (int*)malloc(sizeof(int) * dim2);
    R_number_to_digits(n2, s2, dim2);
 
+   printf("s1 fonte:\n");
+   print_digits(s1,dim1);
+   printf("s2 fonte:\n");
+   print_digits(s2,dim2);
+
    // main function
    K_multip(sum, s1, dim1, s2, dim2);
 
    
    printf("--------------------\n");
-   for (int i = 0; i < (dim1+dim2); i++)
+   for (size_t i = 0; i < (dim1+dim2); i++)
       printf("sum: %d\n", sum[i]);
-   printf("--------------------\n");
 
-   for (int i = 0; i < (dim1+dim2); i++)
-      p += (sum[i] * (int)pow(10,(double)i));
+   for (size_t i = 0; i < (dim1+dim2); i++)
+      p += (sum[i] * (int64_t)pow(10,(double)i));
    
-
-   printf("Result:      %d\n", p);
-   printf("Correct:     %d\n", n1*n2);
-   printf("Difference:  %d\n", (n1*n2) - p);
+   
+   printf("--------------------\n");
+   printf("Result:      %lld\n", p);
+   printf("Correct:     %lld\n", n1*n2);
+   printf("Difference:  %lld\n", (n1*n2) - p);
    printf("--------------------\n");
 
    free(s1);
